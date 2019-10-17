d58312ba0427da3393e52cc5a2010aa066c77410
BK-bokai <bokai830124@gmail.com>
2019-10-17 17:52:00 +0800

    首頁文章編輯完成+資料庫更新

--- bklaravel.sql
+++ bklaravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2019-10-04 23:37:45
+-- 產生時間： 2019-10-17 11:50:45
 -- 伺服器版本： 10.3.16-MariaDB
 -- PHP 版本： 7.3.7
 
@@ -75,15 +75,16 @@ CREATE TABLE `index_photo` (
   `userid` int(11) NOT NULL COMMENT 'po文者id',
   `content_one` text NOT NULL COMMENT '照片第一段內文',
   `content_two` text NOT NULL COMMENT '照片第二段內文',
-  `create_time` datetime NOT NULL COMMENT '上傳時間'
+  `created_at` datetime NOT NULL COMMENT '上傳時間',
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `index_photo`
 --
 
-INSERT INTO `index_photo` (`id`, `title`, `photo_path`, `username`, `userid`, `content_one`, `content_two`, `create_time`) VALUES
-(1, '去夏威夷玩時的照片', 'C:\\xampp\\htdocs\\php\\bkLaravel\\public\\img\\圖片1.jpg', 'Bokai', 1, '大學以及研究所就讀大氣科學系，除了簡單的天氣分析以及預報外，也同時 學到許多在於研究以及應用上的知識與技巧，如：空氣汙染概論、大氣動力學、天氣學、數值天氣預報、大氣測計學等。', '大學期間擔任過校內氣象台的主播，加強對於天氣預報及天氣分析的訓練，不只要將未來的天氣概況做個簡單的預報，並要練習上台講話的台風。研究所期間與實驗室同仁一同參與過許多大大小小的實驗計畫，如西南氣流實驗及雙北暴雨實驗，這些經驗令我體會團隊合作以及團隊討論是進步最快的捷徑。', '2019-08-09 04:14:25');
+INSERT INTO `index_photo` (`id`, `title`, `photo_path`, `username`, `userid`, `content_one`, `content_two`, `created_at`, `updated_at`) VALUES
+(1, '去夏威夷玩時的照片', 'C:\\xampp\\htdocs\\php\\bkLaravel\\public\\img\\圖片1.jpg', 'Bokai', 1, '大學以及研究所就讀大氣科學系，除了簡單的天氣分析以及預報外，也同時 學到許多在於研究以及應用上的知識與技巧，如：空氣汙染概論、大氣動力學、天氣學、數值天氣預報、大氣測計學等ssss。', '大學期間擔任過校內氣象台的主播，加強對於天氣預報及天氣分析的訓練，不只要將未來的天氣概況做個簡單的預報，並要練習上台講話的台風。研究所期間與實驗室同仁一同參與過許多大大小小的實驗計畫，如西南氣流實驗及雙北暴雨實驗，這些經驗令我體會團隊合作以及團隊討論是進步最快的捷徑ssss。', '2019-08-09 04:14:25', '2019-10-17 09:43:38');
 
 -- --------------------------------------------------------
 
@@ -105,7 +106,8 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `user_id`, `body`, `created_at`, `updated_at`, `published_at`) VALUES
-(8, 30, 'qwqdq', '2019-10-03 15:18:46', '2019-10-03 15:18:46', NULL);
+(8, 30, 'testtest', '2019-10-03 15:18:46', '2019-10-05 05:58:07', NULL),
+(23, 30, 'dasd\nasdas\ndad', '2019-10-05 07:30:17', '2019-10-05 07:48:55', NULL);
 
 -- --------------------------------------------------------
 
@@ -128,7 +130,10 @@ CREATE TABLE `message_reply` (
 INSERT INTO `message_reply` (`id`, `message_id`, `reply_id`, `created_at`, `updated_at`) VALUES
 (35, 8, 39, '2019-10-03 15:19:03', '2019-10-03 15:19:03'),
 (36, 8, 40, '2019-10-03 15:19:07', '2019-10-03 15:19:07'),
-(37, 8, 41, '2019-10-03 15:19:10', '2019-10-03 15:19:10');
+(37, 8, 41, '2019-10-03 15:19:10', '2019-10-03 15:19:10'),
+(39, 23, 43, '2019-10-05 08:08:54', '2019-10-05 08:08:54'),
+(40, 23, 44, '2019-10-05 08:12:02', '2019-10-05 08:12:02'),
+(41, 23, 45, '2019-10-05 08:13:54', '2019-10-05 08:13:54');
 
 -- --------------------------------------------------------
 
@@ -193,9 +198,12 @@ CREATE TABLE `replies` (
 --
 
 INSERT INTO `replies` (`id`, `user_id`, `body`, `created_at`, `updated_at`) VALUES
-(39, 30, 'dwdw', '2019-10-03 15:19:03', '2019-10-03 15:19:03'),
+(39, 30, 'tesdt\ndasd\nasdas', '2019-10-03 15:19:03', '2019-10-05 07:53:46'),
 (40, 30, 'dwqdq', '2019-10-03 15:19:07', '2019-10-03 15:19:07'),
-(41, 30, 'wdqd', '2019-10-03 15:19:10', '2019-10-03 15:19:10');
+(41, 30, 'wdqd', '2019-10-03 15:19:10', '2019-10-03 15:19:10'),
+(43, 30, '你好可以\n跟你出去\n玩科科', '2019-10-05 08:08:54', '2019-10-05 08:38:55'),
+(44, 30, '嗨\n你好\n我在這', '2019-10-05 08:12:02', '2019-10-05 08:12:17'),
+(45, 30, '又\n我\n在\\\n這', '2019-10-05 08:13:54', '2019-10-05 08:14:05');
 
 -- --------------------------------------------------------
 
@@ -207,15 +215,16 @@ CREATE TABLE `student` (
   `id` int(11) NOT NULL COMMENT 'id',
   `content` text NOT NULL COMMENT '學生時期自傳',
   `modify_date` datetime DEFAULT NULL COMMENT '修改時間',
-  `create_date` datetime NOT NULL COMMENT '上傳時間'
+  `created_at` datetime NOT NULL COMMENT '上傳時間',
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `student`
 --
 
-INSERT INTO `student` (`id`, `content`, `modify_date`, `create_date`) VALUES
-(1, '研究所期間，除了參加實驗計劃，同時也要資料的處理及分析，因此需要學習程式語言，系上的關系，我們需要在Linux系統下使用shell script、Fortran及GRADS等語言去處理資料及繪圖，在處理資料的過程中，我發現我對於程式語言充滿了興趣，喜歡專研程式技術勝過於學術上的學習。', NULL, '2019-08-09 05:14:15');
+INSERT INTO `student` (`id`, `content`, `modify_date`, `created_at`, `updated_at`) VALUES
+(1, '研究所期間，除了參加實驗計劃，同時也要資料的處理及分析，因此需要學習程式語言，系上的關系，我們需要在Linux系統下使用shell script、Fortran及GRADS等語言去處理資料及繪圖，在處理資料的過程中，我發現我對於程式語言充滿了興趣，喜歡專研程式技術勝過於學術上的學習ssss。', NULL, '2019-08-09 05:14:15', '2019-10-17 09:43:28');
 
 -- --------------------------------------------------------
 
@@ -226,17 +235,18 @@ INSERT INTO `student` (`id`, `content`, `modify_date`, `create_date`) VALUES
 CREATE TABLE `student_skills` (
   `id` int(11) NOT NULL COMMENT 'id',
   `skill_name` varchar(100) NOT NULL COMMENT 'skill_name',
-  `create_time` datetime NOT NULL COMMENT '上傳時間'
+  `created_at` datetime NOT NULL COMMENT '上傳時間',
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `student_skills`
 --
 
-INSERT INTO `student_skills` (`id`, `skill_name`, `create_time`) VALUES
-(2, 'Fortran', '2019-08-09 03:10:00'),
-(3, 'GRADS', '2019-08-09 05:13:13'),
-(8, 'shell script', '2019-08-13 21:11:55');
+INSERT INTO `student_skills` (`id`, `skill_name`, `created_at`, `updated_at`) VALUES
+(2, 'Fortran', '2019-08-09 03:10:00', NULL),
+(3, 'GRADS', '2019-08-09 05:13:13', NULL),
+(8, 'shell script', '2019-08-13 21:11:55', NULL);
 
 -- --------------------------------------------------------
 
@@ -264,7 +274,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `active`, `remember_token`, `reset_token`, `level`, `created_at`, `updated_at`) VALUES
 (1, '', 'admin', '', '$2y$10$ANv/TnPhzaqOwey.frx0NOBmucwDWfp9yhrMk05.kWUGCDYzG8X4G', '0', NULL, NULL, 0, NULL, NULL),
-(30, 'bokai', 'bokai', 'bokai830124@gmail.com', '$2y$10$E5ehMG.x8XPYQ2Kq5o3KGOYx8UkgpAtn/dZzZ2p.WzbsqoUnQms/q', 'active', NULL, NULL, 3, '2019-10-03 14:45:41', '2019-10-03 14:45:58');
+(30, 'bokai', 'bokai', 'bokai830124@gmail.com', '$2y$10$E5ehMG.x8XPYQ2Kq5o3KGOYx8UkgpAtn/dZzZ2p.WzbsqoUnQms/q', 'active', NULL, NULL, 3, '2019-10-03 14:45:41', '2019-10-03 14:45:58'),
+(35, 'tesrt', 'dsadas', 'wd@sda.sadsa', '$2y$10$mPeWTdfZL4QA3I9oiFKq2e3M3tYnruzRoceMlDnp1r2FtHuPxVTBe', 'active', NULL, NULL, 3, '2019-10-08 03:57:11', '2019-10-08 03:57:11'),
+(38, 'root', 'root', 'we@wee.ewqwe', '$2y$10$kHyyBDZit.yD44LZlgRrTudQ.q855TLh56Kjmz8slFiLyYxDcrsEO', 'active', NULL, NULL, 1, '2019-10-08 09:05:21', '2019-10-08 09:05:21');
 
 -- --------------------------------------------------------
 
@@ -276,15 +288,16 @@ CREATE TABLE `worker` (
   `id` int(11) NOT NULL COMMENT 'id',
   `content` text NOT NULL COMMENT '自傳',
   `modify_date` datetime DEFAULT NULL COMMENT '修改時間',
-  `create_date` datetime NOT NULL COMMENT '上傳時間'
+  `created_at` datetime NOT NULL COMMENT '上傳時間',
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `worker`
 --
 
-INSERT INTO `worker` (`id`, `content`, `modify_date`, `create_date`) VALUES
-(1, '退伍出社會後，也順利的進入到現在公司(景丰科技)， 在公司內主要是在Linux系統下建置Weather Research and Forecasting model(WRF model)， 並利用學校所學，處理氣象及WRF model資料，在工作期間我對於程式語言的熱情不減反增， 因此開始自學HTML、CSS、Javascript、Jquery、PHP，並同時使用這些工具開始練習寫一些網站。', NULL, '2019-08-09 03:08:12');
+INSERT INTO `worker` (`id`, `content`, `modify_date`, `created_at`, `updated_at`) VALUES
+(1, '退伍出社會後，也順利的進入到現在公司(景丰科技)， 在公司內主要是在Linux系統下建置Weather Research and Forecasting model(WRF model)， 並利用學校所學，處理氣象及WRF model資料，在工作期間我對於程式語言的熱情不減反增， 因此開始自學HTML、CSS、Javascript、Jquery、PHP，並同時使用這些工具開始練習寫一些網站sssss。', NULL, '2019-08-09 03:08:12', '2019-10-17 09:43:41');
 
 -- --------------------------------------------------------
 
@@ -295,19 +308,20 @@ INSERT INTO `worker` (`id`, `content`, `modify_date`, `create_date`) VALUES
 CREATE TABLE `work_skills` (
   `id` int(11) NOT NULL COMMENT 'id',
   `skill_name` varchar(100) NOT NULL COMMENT '技能名稱',
-  `create_time` datetime NOT NULL COMMENT '上傳日期'
+  `created_at` datetime NOT NULL COMMENT '上傳日期',
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `work_skills`
 --
 
-INSERT INTO `work_skills` (`id`, `skill_name`, `create_time`) VALUES
-(1, 'HTML', '2019-08-09 03:09:12'),
-(2, 'CSS', '2019-08-09 04:13:11'),
-(3, 'Javascript', '2019-08-09 04:10:12'),
-(4, 'Jquery', '2019-08-09 03:06:11'),
-(5, 'PHP', '2019-08-09 02:08:09');
+INSERT INTO `work_skills` (`id`, `skill_name`, `created_at`, `updated_at`) VALUES
+(1, 'HTML', '2019-08-09 03:09:12', NULL),
+(2, 'CSS', '2019-08-09 04:13:11', NULL),
+(3, 'Javascript', '2019-08-09 04:10:12', NULL),
+(4, 'Jquery', '2019-08-09 03:06:11', NULL),
+(5, 'PHP', '2019-08-09 02:08:09', NULL);
 
 --
 -- 已傾印資料表的索引
@@ -418,13 +432,13 @@ ALTER TABLE `index_photo`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `message_reply`
 --
 ALTER TABLE `message_reply`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `migrations`
@@ -436,7 +450,7 @@ ALTER TABLE `migrations`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `replies`
 --
 ALTER TABLE `replies`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `student`
@@ -454,7 +468,7 @@ ALTER TABLE `student_skills`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `worker`
